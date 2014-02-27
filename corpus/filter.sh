4b5b84b33633f66a1fedba9c5955cf5692cc89d3
jeeteshm <jeetesh.mangwani@gmail.com>
2014-02-27 23:41:29 +0530

    filter the corpus

---
+++ corpus/filter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sed 's/[A-Z][ \n\r\t]/A /g'\
+| sed "s/[A-Z]$/A\'/g"\
+| sed 's/[A-Z][A-Z][ \n\r\t]/AB /g'\
+| sed "s/[A-Z][A-Z]$/AB\'/g"\
+| sed 's/[A-Z][A-Z][A-Z][ \n\r\t]/ABC /g'\
+| sed "s/[A-Z][A-Z][A-Z]$/ABC\'/g"\
+| sed 's/ +/ /g'\
+| sed 's/ $//g'\
+| sed 's/[0-9]\+\(\.[0-9]\+\)\?/1.1/g'
