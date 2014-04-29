2503504a724387c52efe208d183d7c90f1e64294
Pankaj Prateek <pankaj200292@gmail.com>
2014-04-29 20:16:57 +0530

    negative have minus before them

--- interpreter/context.c
+++ interpreter/context.c
@@ -59,7 +59,11 @@ double stod(char str[]) {
   bool decimal = false;
   double n = 0;
   double power = 1;
-  for(i=0;i<length;i++) {
+  if(str[0] == '-')
+    i=1;
+  else
+    i=0;
+  for(;i<length;i++) {
     if(str[i] == '.') {
       decimal = true;
       power = 10;
@@ -72,6 +76,8 @@ double stod(char str[]) {
       n = n*10 + (str[i] - '0');
     }
   }
+  if(str[0] == '-')
+    n*=(-1);
   return n;
 }
 
@@ -91,15 +97,25 @@ SplitString split(char str[], char delim) {
   splitstr.length = 0;
   int runcount = 0;
   for(i=0;i<length;i++) {
-    if(str[i] == delim) {
+    if(str[i] == ' ') {
       int j;
-      for(j=runcount;j<=i;j++) {
+      for(j=runcount;j<i;j++) {
 	splitstr.array[splitstr.length][j-runcount] = str[j];
       }
+      splitstr.array[splitstr.length][j-runcount] = '\0';
       runcount = i+1;
       splitstr.length++;
     }
   }
+
+  int j;
+  for(j=runcount;j<i-1;j++) {
+    splitstr.array[splitstr.length][j-runcount] = str[j];
+  }
+  splitstr.array[splitstr.length][j-runcount] = '\0';
+  runcount = i+1;
+  splitstr.length++;
+
   return splitstr;
 }
 
